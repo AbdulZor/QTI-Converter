6a05068ea51ea66a6d8973291d6e7528d19feb9f
AbdulZor <abdulzor@outlook.com>
2020-03-25 14:47:53 +0100

    test for pull request

--- src/main/java/open/edx/qticonverter/services/CourseService.java
+++ src/main/java/open/edx/qticonverter/services/CourseService.java
@@ -537,6 +537,7 @@ public class CourseService {
         outcomeMaxDeclaration.appendChild(maxDefaultValue);
         assessmentItemNode.appendChild(outcomeMaxDeclaration); //append to assessmentItem
 
+
         // setup itemBody element
         Element itemBody = doc.createElement("itemBody");
         Element responseProcessing = doc.createElement("responseProcessing");
